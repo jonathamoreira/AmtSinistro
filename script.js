ef90e898a230d141982ff31d165b65d1dca5e6b1
Jonatha Moreira <johnninajoao@gmail.com>
2022-11-28 20:18:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//colocar data e hora na tela
+var timeDisplay = document.getElementById("time");
+
+function refreshTime() {
+  var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
+  var formattedString = dateString.replace(", ", " - ");
+  timeDisplay.innerHTML = formattedString;
+}
+setInterval(refreshTime, 1000);
